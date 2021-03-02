7ab43922efd9c4218c5341a765b7651f5624f160
rafael-szmarowski <77102488+rafael-szmarowski@users.noreply.github.com>
2021-03-02 16:36:58 +0100

    filtre type rating priceFork package picto

--- routes/index.js
+++ routes/index.js
@@ -14,26 +14,103 @@ router.get('/', function(req, res, next) {
 utiliser pour page accueil, filtres salon (map et liste)
 --> reducer des données du filtre entre la liste, la map et le detail coiffeur qui servira à remplir le fetch à la bdd
 */
-router.post('/search', function(req, res, next) {
+router.post('/search', async function(req, res, next) {
 
-  let type = {$exists: true}
-  let ou = {$exists: true}
+  let type = {$exists: true};
+  let latitude = {$exists: true}
+  let longitude = {$exists: true}
   let quand = {$exists: true}
   let quoi = {$exists: true}
   let package = {$exists: true}
   let picto = {$exists: true}
-  let rating = {$exists: true}
+  let rating = 0;
+  let priceFork = 0;
 
-  req.body.type ? type = req.body.type : null;
-  req.body.ou ? ou = req.body.ou : null;
-  req.body.quand ? quand = req.body.quand : null;
+  req.body.type === 'chez toi' ? type = true : null;
+  req.body.rating ? rating = req.body.rating : null;
+  req.body.priceFork ? priceFork = req.body.priceFork : null;
   req.body.quoi ? quoi = req.body.quoi : null;
   req.body.package ? package = req.body.package : null;
   req.body.picto ? picto = req.body.picto : null;
-  req.body.rating ? rating = req.body.rating : null;
-
+  req.body.latitude ? latitude = req.body.latitude : null;
+  req.body.longitude ? longitude = req.body.longitude : null;
   
+  req.body.quand ? quand = req.body.quand : null;
+  
+  // console.log("req.body.type", req.body.type)
+  // console.log("type", type)
 
+  // console.log("req.body.picto", req.body.picto)
+  // console.log("picto", picto)
+
+  // console.log("req.body.quoi", req.body.quoi)
+  // console.log("quoi", quoi)
+
+  console.log("req.body.latitude", req.body.latitude)
+  console.log("req.body.longitude", req.body.longitude)
+
+  console.log("req.body.latitude", req.body.latitude)
+
+
+  var shopsList = await ShopModel.find(
+    {
+      offers:  { $elemMatch:
+        {
+          type: quoi,
+        }
+      }, 
+      packages: { $elemMatch:
+        {
+          type: package,
+        }
+      }, 
+      atHome: type,
+      rating: {$gt:rating},
+      priceFork: {$gt:priceFork},
+      shopFeatures: picto,
+    }
+  );
+
+  // Filter with distance the result
+
+  let distanceMax = 10;
+  
+    function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2)
+  {
+    var R = 3963; // Radius of the earth in miles
+    var dLat = deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = deg2rad(lon2-lon1);
+    var a =
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+      Math.sin(dLon/2) * Math.sin(dLon/2)
+      ;
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c; // Distance in miles
+    return d;
+  }
+
+  function deg2rad(deg) {
+    return deg * (Math.PI/180)
+  }
+
+  let filteredDistanceShopsList = []
+
+  for (let i = 0; i<shopsList.length; i++) {
+    if (req.body.longitude != null && req.body.latitude != null ) {
+    let distance = Math.floor(getDistanceFromLatLonInKm(latitude, longitude, shopsList[i].latitude, shopsList[i].longitude))
+    if (distance < distanceMax) {
+      filteredDistanceShopsList.push(shopsList[i])
+    }
+  } else {
+    filteredDistanceShopsList.push(shopsList[i])
+  }
+}
+
+
+
+
+  res.json({filteredDistanceShopsList})
 });
 
 // route pour enregistrer les shops via postman
